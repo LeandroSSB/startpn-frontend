ebb7b1b7824888ca42e299fa18fb8b496e713bc1
leandrossb <leandroz3000@gmail.com>
2023-03-31 04:46:09 -0300

    feat: setting context structure for user informations

---
+++ src/context/user/index.jsx
@@ -0,0 +1,28 @@
+import { createContext, useContext, useState } from "react";
+import api from "../../services/api";
+
+
+
+const userContext = createContext({})
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(() => {
+    const user = api.get()
+    return user 
+  })
+
+  const getUser = () => {
+
+  }
+
+  const updateUser = (user) => {
+
+  }
+
+  return (
+    <userContext.Provider value={{user, getuser, updateUser }} >
+
+    </userContext.Provider>
+  )
+}
+
+export const useUser = () => useContext(userContext)
